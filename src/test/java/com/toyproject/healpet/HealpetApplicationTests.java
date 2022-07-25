294412a446c722eecc1c81635fc1fc0636e7756c
gpfksdlrn <gpfksdlrn@naver.com>
2022-07-25 22:09:07 +0900

    firstCommit

---
+++ src/test/java/com/toyproject/healpet/HealpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.healpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
