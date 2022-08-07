f91be1970f71b4d6b4c5ac9715c5907c5bbb4fe8
gpfksdlrn <gpfksdlrn@naver.com>
2022-08-07 22:23:02 +0900

    glt push test!

--- src/main/java/com/toyproject/healpet/HealpetApplication.java
+++ src/main/java/com/toyproject/healpet/HealpetApplication.java
@@ -10,6 +10,7 @@ public class HealpetApplication {
 		SpringApplication.run(HealpetApplication.class, args);
 		String test = "Hello world!!";
 		System.out.println(test + "2");
+		System.out.println(test + "3");
 	}
 
 }
